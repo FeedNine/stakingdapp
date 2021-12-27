e65c0a0bd6b6d829e8a3ec4b8c9ccaafe373f388
broccolistake <96707536+broccolistake@users.noreply.github.com>
2021-12-27 11:07:12 +0800

    git add --all

---
+++ static/js/components/TopInfoSection.js
@@ -0,0 +1,26 @@
+
+const TopInfoSection = () => {
+    return (
+        <div className='container'>
+            <div className='row justify-content-between mt-3'>
+                <div className='col-md-6 top-holder'>
+                    <h1 className='page-title'>Broccoli Club</h1>
+                    <p className='subtitle'>Welcome to the Broccoli Nursery! Give your Broccolis into the nursery and the botanists will reward you every 12 hours with $BRCC!.</p>
+                    <p className='primary-text'>Stake your Broccolis to earn $BRCC  -  Utility Token for the Broccoli Metaverse - Breed your Broccolis with $BRCC in February</p>
+                    <div className='d-flex flex-wrap align-items-center gap-3 my-4 social-holder'>
+                        <a href='https://twitter.com/NFT_Broccoli' target='_blank'><img src='/assets/images/twitter.png' height='45' /></a>
+                        <a href='https://discord.gg/h8T6p8qgQT' target='_blank'><img src='/assets/images/discord.png' height='45' /></a>
+                        <a href='https://opensea.io/collection/broccoli-club' target='_blank'><img src='/assets/images/opensea.png' height='45' /></a>
+                        <a href='https://polygonscan.com/address/0xc84ac19ee8856a0f2879a6a1dfcd1225a9057088' target='_blank'><img src='/assets/images/matic.svg' height='45' /></a>
+                       
+                    </div>
+                </div>
+                <div className='col-md-6 logo-holder'>
+                    <img src='/assets/images/blid.jpg' className='blid-image' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TopInfoSection
